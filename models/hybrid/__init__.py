35455e9d987e5691330ac6173d488c919eaf72b5
MinqiShao <1179731085@qq.com>
2024-06-17 09:03:40 +0900

    new: log for train

---
+++ models/hybrid/__init__.py
@@ -0,0 +1,1 @@
+from .Quanv_iswap import QCNNi
--- tq_models/__init__.py
+++ tq_models/__init__.py
@@ -3,3 +3,4 @@ from .pure_multi import MultiEncoding_
 from .CCQC import CCQC_
 from .QCL import QCL_
 from .QCNN import QCNN
+from .Quanv_iswap import QCNNi_
